b9cb0109dd7cf49c00599b564928609baac61ff5
René Filip <renefilip@mail.com>
2017-05-31 19:34:01 +0200

    submit solution

--- week4/problemD/problemD.cpp
+++ week4/problemD/problemD.cpp
@@ -6,7 +6,6 @@
 #include <queue>
 #include <algorithm>
 #include <set>
-
 /*
     Corner cases to think about:
     1) no supermarket at all (s == 0)
@@ -53,20 +52,9 @@ void printPred(const std::vector<node_t> &pred, int n)
     std::cout << "\n";
 }
 
-weight_t findDist(const node_t node1, const node_t node2, const graph_t &graph)
-{
-    for (const auto &edge : graph[node1])
-    {
-        if (edge.to == node2)
-            return edge.weight;
-    }
-    return weightMax;
-}
-
-weight_t dijkstra(const node_t start, const node_t end, const graph_t &graph, const int n, std::vector<node_t> &pred)
+void dijkstra(const node_t start, const graph_t &graph, const int n, std::vector<node_t> &pred, std::vector<weight_t> &distance)
 {
     std::vector<bool> visited(n+1);
-    std::vector<weight_t> distance(n+1);
 
     for (int i=1; i<=n; ++i)
     {
@@ -111,8 +99,6 @@ weight_t dijkstra(const node_t start, const node_t end, const graph_t &graph, co
         // printVisited(visited, n);
         // printPred(pred, n);
     }
-
-    return distance[end];
 }
 
 void printTime(weight_t mins)
@@ -135,7 +121,6 @@ void printTime(weight_t mins)
 
 int main()
 {
-    printTime(weightMax);
     std::ios_base::sync_with_stdio(false);
 
     int t;
@@ -144,6 +129,7 @@ int main()
     for (int testCase=1; testCase <= t; ++testCase)
     {
         std::cout << "Case #" << testCase << ": ";
+        // std::cout << "\n";
 
         int n, m, s, a, b;
         std::cin >> n; // number of cities, 1 ... n
@@ -174,6 +160,8 @@ int main()
         }
 
         // if there aren't any supermarkets, we can straight continue
+
+
         if (s == 0)
         {
             std::cout << "impossible\n";
@@ -181,77 +169,70 @@ int main()
         }
 
         // TODO see solution above
-        std::vector<node_t> pred (n+1);
-        weight_t shortestPath = dijkstra(a, b, graph, n, pred); // from lea to peter
-        weight_t shortestPath = dijkstra(b, a, graph, n, pred2); // calc this since there might be a shorter way to peter from the supermarket
-        // printPred(pred, n);
-        if (shortestPath == weightMax)
-        {
-            std::cout << "impossible\n";
-            continue;
-        }
+        std::vector<node_t> pred_lea (n+1);
+        std::vector<weight_t> distance_lea (n+1);
+        std::vector<node_t> pred_peter (n+1);
+        std::vector<weight_t> distance_peter (n+1);
 
-        // create set that contains all ffrom the shortest path
-        std::set<node_t> shortestPathMembers;
-        node_t traverseNode = b;
-        shortestPathMembers.insert(traverseNode);
-        do {
-            traverseNode = pred[traverseNode]; // start from peter
-            shortestPathMembers.insert(traverseNode);
-        } while(traverseNode != a);
+        dijkstra(a, graph, n, pred_lea, distance_lea); // shortest path from lea
+        dijkstra(b, graph, n, pred_peter, distance_peter); // shortest path from peter
 
-        // for (auto it=shortestPathMembers.begin(); it!=shortestPathMembers.end(); ++it)
-        //     std::cout << *it << " ";
-        // std::cout << '\n';
+        // printPred(pred_lea, n);
+        // printPred(pred_peter, n);
 
-        weight_t currentMin = weightMax;
-        weight_t currentTime;
-
-        // now add supermarekt times to shortestPath
+        weight_t minDistance = weightMax;
+        weight_t result = weightMax;
         for (const auto &supermarket : supermarkets)
         {
-            if (shortestPathMembers.count(supermarket.city))
+            // std::cout << "supermarket: " << supermarket.city << "\n";
+
+            // weight_t time1 = calcDistance(a, supermarket.city, pred_lea, distance_lea);
+            weight_t time1 = distance_lea[supermarket.city];
+            if (time1 == weightMax)
             {
-                // supermarket city is directlz on the shoretest path
-                // printTime(shortestPath + supermarket.min); // TODO only which are in the shortest path, prec!!
-                currentTime = shortestPath + supermarket.min;
-            }
-            else if (pred[supermarket.city] == 0)
-            {
-                // not reachable. continue
+                // std::cout << "impossible\n";
                 continue;
             }
-            else
+            // std::cout << "time1: " << time1 << "\n";
+            //
+            // std::cout << "supermarket: " << supermarket.min << "\n";
+
+            // if time1 is infinity, then the supermarket is not connected to the shortest path
+
+            weight_t currentDistance = 0;
+            weight_t currentNode = supermarket.city;
+
+            while (pred_peter[currentNode] != 0)
             {
-                // supermarkt is somehwere else... go there and then back to the shortest path
-                // sum up distances until one get passed by the shortest path.
-                // 2 * travserse path + time supermarket
-                // fuck.. traverse again
-                int additionalMins = 0;
-                node_t currentNode = supermarket.city;
-
-                while(!shortestPathMembers.count(currentNode))
-                {
-                    additionalMins += (2 * findDist(currentNode, pred[currentNode], graph));
-                    currentNode = pred[currentNode];
-                }
-
-                currentTime = shortestPath + additionalMins + supermarket.min;
+                // std::cout << "currentNode: " << currentNode << "\n";
+                // std::cout << "pred_peter[currentNode]: " << pred_peter[currentNode] << "\n";
+                currentDistance += std::abs(distance_peter[currentNode] - distance_peter[pred_peter[currentNode]]);
+                // std::cout << "d: " << currentDistance << "\n";
+                currentNode = pred_peter[currentNode];
             }
 
-            if (currentTime < currentMin)
+            if (currentNode != b)
+                continue;
+
+
+            weight_t time2 = currentDistance;
+            // std::cout << "time2: " << time2 << "\n";
+
+            result = time1 + time2 + supermarket.min;
+            if (result < minDistance)
             {
-                currentMin = currentTime;
+                minDistance = result;
             }
 
         }
 
-        if (currentMin == weightMax)
+        if (minDistance == weightMax)
         {
             std::cout << "impossible\n";
         } else {
-            printTime(currentMin);
+            printTime(minDistance);
         }
+
     }
 
     return 0;
